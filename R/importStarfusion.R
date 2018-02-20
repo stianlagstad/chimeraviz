541da57f8967e4d111d153d8615cde8a0f770372
Philip Lijnzaad <plijnzaad@gmail.com>
2018-02-20 13:44:00 +0100

    now also setting fusion@inframe ...
    ... but only if column PROT_FUSION_TYPE is available (i.e. when reading
    star-fusion.fusion_predictions.abridged.annotated.coding_effect.tsv )
    If the (default) "star-fusion.fusion_predictions.abridged.tsv" file
    is used, inframe remains NA (and a warning is issued)
    now also setting fusion@inframe ...

--- R/importStarfusion.R
+++ R/importStarfusion.R
@@ -52,7 +52,8 @@ importStarfusion <- function (filename, genomeVersion, limit) {
         "LeftBreakEntropy" = col_number(),
         "RightBreakDinuc" = col_character(),
         "RightBreakEntropy" = col_number(),
-        "FFPM" = col_number()
+        "FFPM" = col_number(),
+        "PROT_FUSION_TYPE" = col_character()
       )
       if (missing(limit)) {
         # Read all lines
@@ -103,6 +104,17 @@ importStarfusion <- function (filename, genomeVersion, limit) {
     fusionToolSpecificData[["RightBreakEntropy"]] = report[[i, "RightBreakEntropy"]]
     fusionToolSpecificData[["FFPM"]] = report[[i, "FFPM"]]
 
+    if(!is.null(report$PROT_FUSION_TYPE)) {
+        ## only available when loading from coding_effect file (i.e.
+        ## star-fusion.fusion_predictions.abridged.annotated.coding_effect.tsv)
+        ft <- report[[i, "PROT_FUSION_TYPE"]]
+        fusionToolSpecificData[["inframe"]] <- ft
+        if (ft == 'INFRAME')
+          inframe <- TRUE
+        if (ft == 'FRAMESHIFT')
+          inframe <- FALSE
+    }
+    
     # id for this fusion
     id <- as.character(i)
 
