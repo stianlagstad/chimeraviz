7319571280592a1225abf41c7a475b375d065a9e
Stian Lågstad <stianlagstad@gmail.com>
2020-03-10 13:30:47 +0100

    Duplicate test for show_all_nucleotides=FALSE

--- tests/testthat/test_plot_fusion_reads.R
+++ tests/testthat/test_plot_fusion_reads.R
@@ -39,3 +39,25 @@ test_that("plot_fusion_reads produces a png file", {
   # See if we actually produced a file with size > 0
   expect_equal(file.info(png_filename)$size > 0, TRUE)
 })
+
+test_that("plot_fusion_reads with show_all_nucleotides=FALSE produces a png file", {
+  # Expect error when we haven't added the fusion reads bam file to the fusion
+  # object yet
+  expect_error(plot_fusion_reads(fusion, png_filename))
+
+  # Add the bam file of aligned fusion reads to the fusion object
+  fusion <- add_fusion_reads_alignment(fusion, bamfile)
+  # Calculate image size based on supporting reads and lenght of junction
+  # sequence.
+  image_width <- (nchar(fusion@gene_upstream@junction_sequence) +
+    nchar(fusion@gene_downstream@junction_sequence)) * 10
+  image_height <- (fusion@split_reads_count + fusion@spanning_reads_count) * 20
+  # Open device
+  png(png_filename, width = image_width, height = image_height)
+  # Now we can plot
+  plot_fusion_reads(fusion, show_all_nucleotides=FALSE)
+  # Close device
+  dev.off()
+  # See if we actually produced a file with size > 0
+  expect_equal(file.info(png_filename)$size > 0, TRUE)
+})
